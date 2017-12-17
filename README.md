9be1239020b08c519f3df6315a9ecec8536d6aea
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-12-17 02:16:14 -0800

    initial push, updating readme and adding code written before creation of the git project

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # math-interpreter
-Basic calculator program running from console.
+###### Basic calculator program running from console. ######
+
+### Instructions
+
+In order to use this program, call it using your favorite command line interface.
+
+Upon running, a helpful prompt will guide you through the structuring of your math problem.
+
+First you will be asked what kind of problem is being run. math-interpreter supports multiplication, division, addition and subtraction. Enter "\*", "/", "+", or "-" to progress.
+
+After this you will be prompted to enter the first number in your equation. math-interpreter supports doubles, so enter a number with or without a decimal to continue.
+
+After this you will be prompted to enter the second number in your equation. Upon completion of this request, an answer will be given.
+
+If further math is required, please write down your answer on a notepad or peice of paper before pressing enter to close the application. Re-run the application as needed to produce subsequent results. When you are finished, stop running the program, turn off your computer, disconnect the power cable from the back of your computer and turn off the lights to conserve power.
+
+You have completed math. Give yourself a hearty round of applause.
+
+### About math-interpreter.
+
+math_interpreter was written in way too many hours by Dustin Mundy, a not-yet graduate of the Codefellows 401 course in Seattle Washington specializing in ASP.NET.
+
+Mind that at the time of writing, this application is not actually functional, and defies all logic as it appears to run several methods at once, prompting for several items at once and responding with a failure text only after two values have been entered. The content of these values do not appear to matter.
+
+While Dustin learned quite a lot in developing this console application, C# is crazy and will require much more time to master.
+
+This mastery had better come quick, as at the time of writing, even googling code snippits to assist in building this application has produced results that are "way over (the author's) head."
---
+++ math_interpreter/math_interpreter/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace math_interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char oper = 'e';
+            double number1 = 0.0;
+            double number2 = 0.0;
+            while (oper != 'e')
+            {
+                oper = Program.GetOper();
+            }
+            bool num1finished = false;
+            while (!num1finished)
+            {
+                num1finished = Program.GetNumber1(oper, number1);
+            }
+            bool num2finished = false;
+            while (!num2finished)
+            {
+                num2finished = Program.GetNumber2(oper, number2);
+            }
+            double answer = 0;
+            switch (oper)
+            {
+                case '*':
+                    answer = number1 * number2;
+                    break;
+                case '/':
+                    answer = number1 / number2;
+                    break;
+                case '+':
+                    answer = number1 + number2;
+                    break;
+                case '-':
+                    answer = number1 - number2;
+                    break;
+            }
+            Console.WriteLine("Your answer is " + answer + ".");
+            Console.ReadLine();
+        } // End Main
+
+        private static char GetOper()
+        {
+            Console.WriteLine("What type of operation are we trying today?");
+            Console.WriteLine("(You can enter things like Sum, +, /, or Multiply)");
+            string operatorinput = Console.ReadLine();
+            switch (operatorinput)
+            {
+                case "*":
+                case "multiply":
+                case "product":
+                    return ('*');
+                case "/":
+                case "divide":
+                case "quotient":
+                    return ('/');
+                case "+":
+                case "add":
+                case "sum":
+                    return ('+');
+                case "-":
+                case "subtract":
+                case "difference":
+                    return ('-');
+                default:
+                    Console.WriteLine("What was that? I don't understand what " + operatorinput + "is.");
+                    return ('e');
+            } // End Switch
+        }
+
+
+
+        private static bool GetNumber1(char oper, double number1)
+        {
+            Console.WriteLine("Hello! What type of operation are we trying today?");
+            Console.WriteLine("(You can enter things like Sum, +, /, or Multiply)");
+            string operatorinput = Console.ReadLine();
+            switch (oper)
+            {
+                case '*':
+                    Console.Write("Good! Now what is the first number we're multiplying?");
+                    break;
+                case '/':
+                    Console.Write("Good! Now what is the number we're dividing?");
+                    break;
+                case '+':
+                    Console.Write("Good! Now what is the first number we're adding?");
+                    break;
+                case '-':
+                    Console.Write("Good! Now what is the number being subracted from?");
+                    break;
+            }
+            string inputnumber1 = Console.ReadLine();
+            if (Double.TryParse(inputnumber1, out number1))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("That doesn't seem to be a valid number. Try again?");
+                return false;
+            }
+        }
+
+        private static bool GetNumber2(char oper, double number2)
+        {
+            Console.WriteLine("Hello! What type of operation are we trying today?");
+            Console.WriteLine("(You can enter things like Sum, +, /, or Multiply)");
+            string operatorinput = Console.ReadLine();
+            switch (oper)
+            {
+                case '*':
+                    Console.WriteLine("Good, and what nunber are we multiplying that by?");
+                    break;
+                case '/':
+                    Console.WriteLine("Good, and what nunber are we dividing that by?");
+                    break;
+                case '+':
+                    Console.WriteLine("Good, and what nunber are we adding that to?");
+                    break;
+                case '-':
+                    Console.WriteLine("Good, and what nunber are we subtracting?");
+                    break;
+            }
+            string inputnumber1 = Console.ReadLine();
+            if (Double.TryParse(inputnumber1, out number2))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("That doesn't seem to be a valid number. Try again?");
+                return false;
+            }
+        }
+    }
+}
